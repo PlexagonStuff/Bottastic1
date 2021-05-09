1b4ae7e6740a06a4ef9cc9131cd7626ae76ffae5
Plex <83173454+PlexagonStuff@users.noreply.github.com>
2021-05-09 11:44:51 -0400

    Changes!

--- bottastic1/Gift.gd
+++ bottastic1/Gift.gd
@@ -1,7 +1,8 @@
 extends Gift
 var skits = ["https://www.youtube.com/watch?v=6Mce7WTxbSo","https://www.youtube.com/watch?v=Zcf-XoDtgqk","https://www.youtube.com/watch?v=KJbHrZVTduQ","https://www.youtube.com/watch?v=jHfsWc1nUx8","https://www.youtube.com/watch?v=NUrghhh9mM8","https://www.youtube.com/watch?v=lIGueEYDU8M","https://www.youtube.com/watch?v=ZMJKeUT8_Wg","https://www.youtube.com/watch?v=1POEZ1x37WM","https://www.youtube.com/watch?v=d_D4n3CcHCQ","https://www.youtube.com/watch?v=9wItvEICj0c","https://www.youtube.com/watch?v=kJkaWV5MvPg","https://www.youtube.com/watch?v=Y3Q6Eaq5ZQk","https://www.youtube.com/watch?v=neILZOQNz6s","https://www.youtube.com/watch?v=yu4qvpvZay8","https://www.youtube.com/watch?v=B9Fe9tiyg5M","https://www.youtube.com/watch?v=GrmjSFWiVpg","https://www.youtube.com/watch?v=1YScVDMmpsM"]
 var rng = RandomNumberGenerator.new()
-
+onready var gameTitle = $LineEdit
+var sabi = 0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -38,19 +39,27 @@ func parse_chat_input(player_name: String, message: String, override=false):
 	if message.begins_with("#discord"):
 		chat("Fork's discord is https://www.discord.gg/gZjHjM644u")
 	if message.begins_with("#help"):
-		chat("Commands are youtube, twitter, discord, game, skit, wr, whatthefork, src, instagram. Add a # in front of each word.")
+		chat("Commands are youtube, twitter, discord, game, skit, wr, whatthefork, src, instagram, dltodo, taikotodo. Add a # in front of each word.")
 	if message.begins_with("#game"):
-		chat("Fork is playing Super Mario 64, 3D All Stars")
+		chat("Fork is playing " + gameTitle.text)
 	if message.begins_with("#skit"):
 		chat("Here's a random Forktastic skit! " + skits[rng.randi_range(0, (skits.size() - 1))])
 	if message.begins_with("#wr"):
 		chat("Currently Fork has one WR on speedrun.com")
 	if message.begins_with("#src"):
 		chat("Fork's speedrun.com profile is https://www.speedrun.com/user/Forktastic")
+	if message.begins_with("#dltodo"):
+		chat("Fork's Speedrun To-Do list can be found here: https://docs.google.com/document/d/1sAttG8UqrH68TRb74fEBHIjvX32-9Sihj9xvToDCv_8/edit?usp=sharing")
+	if message.begins_with("#taikotodo"):
+		chat("Fork's Taiko To-Do list can be found here: https://docs.google.com/document/d/1tPk6QEJwn2FQdU_2-B_qlJ7wpqc0oo42O3YkvbgKxxA/edit")
 	if message.begins_with("#instagram"):
 		chat("Fork's insta is https://www.instagram.com/forktastic1/")
 	if message.begins_with("#whatthefork"):
 		chat("What the Fork! is Forktastic's podcast with friends about random subjects,from fruit to the fragility of life. You can view all episodes here: https://www.youtube.com/playlist?list=PLqrBrVyFI2CcMNy4PlFifV8ijjag7sfsD")
+	if player_name == "Sabi2798":
+		if sabi < 2:
+			chat("saaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabi")
+			sabi = sabi + 1
 	if "forktastic" in message:
 		chat("Casual Youtuber that picked up streaming as well because life is short! I predominantly stream Duck Life speedruns, but I'm looking at expanding to Switch games in the future, starting with Taiko (with the drum controller!). So if you like what you're seeing, consider following!")
 		
